13e6fc0c852bee5bf80da6139ba73472b3554b8a
Lucas Holter <lucas.holter@hotmail.com>
2026-01-22 14:05:41 +0100

    Add direnv and nix-direnv

--- home.nix
+++ home.nix
@@ -1,11 +1,23 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 let
+  zsh-syntax-highlighting = pkgs.fetchFromGitHub {
+  owner = "zsh-users";
+  repo = "zsh-syntax-highlighting";
+  rev = "5eb677b";
+  sha256 = "sha256-KRsQEDRsJdF7LGOMTZuqfbW6xdV5S38wlgdcCM98Y/Q=";
+};
+  nvimConfig = pkgs.fetchFromGitHub {
+  owner = "lucasholter00";
+  repo = "barebones-nvim";
+  rev = "2d29ecc";
+  sha256 = "sha256-SVToLRkIndTqh84Mk7ZhBnG0vwM7kAnF2Pi2bxZyHyg=";
+};
   dotfiles = pkgs.fetchFromGitHub {
   owner = "lucasholter00";
   repo = "dotfiles";
-  rev = "95865d6";
-  sha256 = "sha256-/MGnjqTwb+YiwzW/hShZBwTYiB4Af47IvVZb/H7S1xc=";
+  rev = "9f59e63";
+  sha256 = "sha256-rjzA86ncRCRH9BKoZVY1n5BA75UdSUP5BKLcukyF/OQ=";
 };
 in 
 {
@@ -41,7 +53,17 @@ in
     pkgs.tmux
     pkgs.zsh
     pkgs.neovim
-    pkgs.oh-my-zsh
+    pkgs.eza
+    pkgs.zoxide
+    pkgs.curl
+    pkgs.bash
+    pkgs.nodejs_24
+    pkgs.cargo
+    pkgs.gccgo15
+    pkgs.zsh-autosuggestions
+    pkgs.tree
+    pkgs.direnv
+    pkgs.nix-direnv
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
@@ -53,6 +75,21 @@ in
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
+  home.file.".config/nvim" = {
+    source = "${nvimConfig}/";
+    target = ".config/nvim/";
+  };
+
+  home.file.".oh-my-zsh/custom/plugins/zsh-autosuggestions" = {
+    source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
+    target = ".oh-my-zsh/custom/plugins/zsh-autosuggestions" ;
+  };
+
+  home.file.".oh-my-zsh/custom/plugins/zsh-syntax-highlighting" = {
+    source = "${zsh-syntax-highlighting}";
+    target = ".oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ;
+  };
+
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
@@ -69,6 +106,24 @@ in
     ".tmux.conf".source = "${dotfiles}/.tmux.conf";
     ".p10k.zsh".source = "${dotfiles}/.p10k.zsh";
   };
+
+  home.activation = {
+    installOmz = lib.hm.dag.entryAfter ["writeBoundary" "installPackages" "zsh"] ''
+      export PATH="${
+        lib.makeBinPath (with pkgs; [zsh git])
+      }:$PATH"
+      if [ -d "''${HOME}/.oh-my-zsh" ]; then
+        echo "Oh my zsh already installed"
+      else
+        run sh -c "$(${pkgs.curl}/bin/curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh ) --unattended"
+      fi
+      if [ -d "''${HOME}/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+        echo "Powerlevel10k already installed"
+      else
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "''${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+      fi
+    '';
+  };     
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
@@ -89,6 +144,16 @@ in
   home.sessionVariables = {
     # EDITOR = "emacs";
     SHELL = "${pkgs.zsh}/bin/zsh";
+  };
+
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+
+    bash.enable = true; # see note on other shells below
   };
 
   # Let Home Manager install and manage itself.
